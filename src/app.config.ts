9d713c869170aaef93501b26041a0769bb633f84
StephenQiu30 <89136224+StephenQiu30@users.noreply.github.com>
2026-05-24 12:55:53 +0800

    [M3] Taro小程序主链路与OpenAPI接入 (#11)
    * test: 定义小程序OpenAPI客户端契约
    
    * feat: 生成小程序OpenAPI客户端
    
    * test: 定义小程序Taro主链路契约
    
    * feat: 实现Taro小程序主链路

---
+++ src/app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  pages: ["pages/index/index"],
+  window: {
+    navigationBarTitleText: "HotKey",
+    navigationBarBackgroundColor: "#f5c542",
+    navigationBarTextStyle: "black",
+    backgroundColor: "#f8f7f2",
+  },
+});
---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from "react";
+
+import "./app.scss";
+
+export default function App({ children }: PropsWithChildren) {
+  return children;
+}
